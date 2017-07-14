a956c490e45d82f5de0fa4661e7153f9f770fe56
Daniel Chong <danielgq.chong@gmail.com>
2017-07-14 14:59:46 +1200

    able to get jwt token

--- jogging_time_tracker_featherjs/src/services/users/users.hooks.js
+++ jogging_time_tracker_featherjs/src/services/users/users.hooks.js
@@ -1,40 +1,40 @@
-const { authenticate } = require('feathers-authentication').hooks;
-const commonHooks = require('feathers-hooks-common');
-const { restrictToOwner } = require('feathers-authentication-hooks');
-
+const { authenticate } = require('feathers-authentication').hooks
+const commonHooks = require('feathers-hooks-common')
+const { restrictToOwner } = require('feathers-authentication-hooks')
+const local = require('feathers-authentication-local')
 
 const restrict = [
   authenticate('jwt'),
   restrictToOwner({
     idField: '_id',
-    ownerField: '_id'
-  })
-];
+    ownerField: '_id',
+  }),
+]
 
 module.exports = {
   before: {
     all: [],
-    find: [ authenticate('jwt') ],
-    get: [ ...restrict ],
-    create: [  ],
-    update: [ ...restrict ],
-    patch: [ ...restrict ],
-    remove: [ ...restrict ]
+    find: [authenticate('jwt')],
+    get: [...restrict],
+    create: [local.hooks.hashPassword({ passwordField: 'password' })],
+    update: [...restrict],
+    patch: [...restrict],
+    remove: [...restrict],
   },
 
   after: {
     all: [
       commonHooks.when(
         hook => hook.params.provider,
-        commonHooks.discard('password')
-      )
+        commonHooks.discard('password'),
+      ),
     ],
     find: [],
     get: [],
     create: [],
     update: [],
     patch: [],
-    remove: []
+    remove: [],
   },
 
   error: {
@@ -44,6 +44,6 @@ module.exports = {
     create: [],
     update: [],
     patch: [],
-    remove: []
-  }
-};
+    remove: [],
+  },
+}
