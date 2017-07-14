f1e40d0a637ac6251bfb5e7d9d9928e82ef1ffce
Daniel Chong <danielgq.chong@gmail.com>
2017-07-14 13:04:57 +1200

    adding timelogs service

--- jogging_time_tracker_featherjs/src/services/index.js
+++ jogging_time_tracker_featherjs/src/services/index.js
@@ -1,3 +1,5 @@
+const timelogs = require('./timelogs/timelogs.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(timelogs);
 };
