bdf95665b018c12fe15271cf48f5b61c7f31d2e5
Daniel Chong <danielgq.chong@gmail.com>
2017-07-14 13:05:48 +1200

    adding username password

--- jogging_time_tracker_featherjs/src/services/index.js
+++ jogging_time_tracker_featherjs/src/services/index.js
@@ -1,5 +1,7 @@
 const timelogs = require('./timelogs/timelogs.service.js');
+const users = require('./users/users.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(timelogs);
+  app.configure(users);
 };
