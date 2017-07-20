fc044a4b79311acf8acd337048ee2dbf2c339e63
Daniel Chong <danielgq.chong@gmail.com>
2017-07-20 15:27:14 +1200

    able to compare between this week and last week

--- jogging_time_tracker/App/Components/TimeRow/StatisticView.js
+++ jogging_time_tracker/App/Components/TimeRow/StatisticView.js
@@ -53,6 +53,21 @@ const StatisticView = props => {
   const monthSlowestJogRecordText = `Month: `
   const yearSlowestJogRecordText = `Year: `
 
+  const lastWeek = weekList[moment().subtract(7, 'days').isoWeek()]
+  const thisWeek = weekList[moment().isoWeek()]
+
+  const lastWeekDistance = _.sumBy(lastWeek, 'distance')
+  const lastWeekDistanceText = getDistanceText(lastWeekDistance)
+  const lastWeekDuration = _.sumBy(lastWeek, 'duration')
+  const lastWeekDurationText = getDurationText(lastWeekDuration)
+  const lastWeekSpeedText = getSpeedText(lastWeekDistance, lastWeekDuration)
+
+  const thisWeekDistance = _.sumBy(thisWeek, 'distance')
+  const thisWeekDistanceText = getDistanceText(thisWeekDistance)
+  const thisWeekDuration = _.sumBy(thisWeek, 'duration')
+  const thisWeekDurationText = getDurationText(thisWeekDuration)
+  const thisWeekSpeedText = getSpeedText(thisWeekDistance, thisWeekDuration)
+
   const dayList = _.groupBy(newData, 'day')
   const newDayList = _.map(dayList, day => ({
     ...day,
@@ -62,8 +77,6 @@ const StatisticView = props => {
   const greatestDistanceObj = _.maxBy(newDayList, 'distance')
   const greatestDistanceDay = greatestDistanceObj[0].day.format('DD/MM/YYYY')
   const greatestDistance = getDistanceText(greatestDistanceObj.distance)
-
-  const lastWeekComparison = `Last Week`
 
   const totalDistance = _.sumBy(newData, 'distance')
   const moonDistancePercentage = _.round(totalDistance / moonDistance * 100, 3)
@@ -113,6 +126,43 @@ const StatisticView = props => {
         </View>
       </Card>
 
+      <Card title="Last Week Comparison">
+        <View style={styles.row}>
+          <View>
+            <Text />
+            <Text>Distance</Text>
+            <Text>Duration</Text>
+            <Text>Speed</Text>
+          </View>
+
+          <View>
+            <Text>Last Week</Text>
+            <Text>
+              {lastWeekDistanceText}
+            </Text>
+            <Text>
+              {lastWeekDurationText}
+            </Text>
+            <Text>
+              {lastWeekSpeedText}
+            </Text>
+          </View>
+
+          <View>
+            <Text>This Week</Text>
+            <Text>
+              {thisWeekDistanceText}
+            </Text>
+            <Text>
+              {thisWeekDurationText}
+            </Text>
+            <Text>
+              {thisWeekSpeedText}
+            </Text>
+          </View>
+        </View>
+      </Card>
+
       <Card title="Greatest Distance Day">
         <View style={styles.row}>
           <Text>
@@ -122,12 +172,6 @@ const StatisticView = props => {
             {greatestDistance}
           </Text>
         </View>
-      </Card>
-
-      <Card title="Last Week Comparison">
-        <Text>
-          {lastWeekComparison}
-        </Text>
       </Card>
 
       <Card title="Cumulative">
