240dace15e5ff65495abe4e1430bacf958b07e3b
Daniel Chong <danielgq.chong@gmail.com>
2017-07-22 11:05:53 +1200

    added test for the buttons on login view

--- jogging_time_tracker/e2e/LoginViewTest.js
+++ jogging_time_tracker/e2e/LoginViewTest.js
@@ -19,4 +19,25 @@ describe('LoginViewTest', () => {
     await element(by.label('Already registered?')).tap()
     await expect(element(by.label('Login'))).toBeVisible()
   })
+
+  // Buttons
+  it('should click on the admin button', async () => {
+    await element(by.label('Admin')).tap()
+    await expect(element(by.label('admin@test.com'))).toBeVisible()
+  })
+
+  it('should click on the manager button', async () => {
+    await element(by.label('Manager')).tap()
+    await expect(element(by.label('manager@test.com'))).toBeVisible()
+  })
+
+  it('should click on the user1 button', async () => {
+    await element(by.label('User1')).tap()
+    await expect(element(by.label('user1@test.com'))).toBeVisible()
+  })
+
+  it('should click on the user2 button', async () => {
+    await element(by.label('User2')).tap()
+    await expect(element(by.label('user2@test.com'))).toBeVisible()
+  })
 })
