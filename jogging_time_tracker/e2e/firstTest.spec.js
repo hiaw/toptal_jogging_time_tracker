ce20ac57029afa8d2c30b4cdca93ee2666698412
Daniel Chong <danielgq.chong@gmail.com>
2017-07-21 21:35:29 +1200

    first e2e testing that shows user logged in

--- jogging_time_tracker/e2e/firstTest.spec.js
+++ jogging_time_tracker/e2e/firstTest.spec.js
@@ -1,19 +1,28 @@
 describe('Example', () => {
-  beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-  
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.label('Hello!!!'))).toBeVisible();
-  });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.label('World!!!'))).toBeVisible();
-  });
+  before(async () => {
+    await device.reloadReactNative()
+  })
+
+  it('should show login page', async () => {
+    /* await expect(element(by.label('EMAIL'))).toBeVisible()*/
+    /* await expect(element(by.label('PASSWORD'))).toBeVisible()*/
+    await expect(element(by.label('Login'))).toBeVisible()
+    await expect(element(by.label('Not yet registered?'))).toBeVisible()
+  })
+
+  it('should show register when click on no yet register', async () => {
+    await element(by.label('Not yet registered?')).tap()
+    await expect(element(by.label('Register'))).toBeVisible()
+    await expect(element(by.label('Already registered?'))).toBeVisible()
+  })
+
+  it('should return to login page', async () => {
+    await element(by.label('Already registered?')).tap()
+    await expect(element(by.label('Login'))).toBeVisible()
+  })
+
+  it('should login as user 1', async () => {
+    await element(by.label('Login')).tap()
+    await expect(element(by.label('Time Logs'))).toBeVisible()
+  })
 })
