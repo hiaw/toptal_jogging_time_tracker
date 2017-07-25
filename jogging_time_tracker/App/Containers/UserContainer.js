5d530eac7cea66b306b7780a1243ebd1b054968a
Daniel Chong <danielgq.chong@gmail.com>
2017-07-26 11:01:57 +1200

    moved user image related code to separate file

--- jogging_time_tracker/App/Containers/UserContainer.js
+++ jogging_time_tracker/App/Containers/UserContainer.js
@@ -1,24 +1,15 @@
 /* @flow*/
 import { compose, mapProps, withHandlers, withState } from 'recompose'
-import { Platform, Alert } from 'react-native'
+import { Alert } from 'react-native'
 import { reduxForm } from 'redux-form'
 import { Actions } from 'react-native-router-flux'
-import { RNS3 } from 'react-native-aws3'
-import ImageResizer from 'react-native-image-resizer'
-import ImagePicker from 'react-native-image-picker'
 
 import UserView from '../Components/User/UserView.js'
+import UserImageHOC from './UserImageHOC.js'
 
 const catchError = err => {
   console.log(err.message)
   Alert.alert('Error', err.message)
-}
-
-const imagePickerOptions = {
-  title: 'Select Product Image',
-  storageOptions: {
-    cameraRoll: true,
-  },
 }
 
 const UserEditor = compose(
@@ -36,51 +27,9 @@ const UserEditor = compose(
     return props
   }),
   withState('editing', 'setEditing', false),
-  withState('uploading', 'setUploading', false),
-  withState('imageURL', 'setImageURL', ''),
   withState('buttonText', 'setButtonText', 'Edit'),
+  UserImageHOC,
   withHandlers({
-    uploadImage: props => source => {
-      const { setImageURL, user: { _id } } = props
-      const file = {
-        uri: source,
-        name: `${_id}.png`,
-        type: 'image/png',
-      }
-
-      const options = {
-        keyPrefix: 'uploads/',
-        bucket: 'toptal-jogging-timelogger',
-        region: 'ap-southeast-2',
-        accessKey: 'AKIAIDQZRFHOIUE7SL5A',
-        secretKey: '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',
-        successActionStatus: 201,
-      }
-
-      RNS3.put(file, options).then(response => {
-        if (response.status !== 201)
-          throw new Error('Failed to upload image to S3')
-        console.log(response.body)
-      })
-    },
-  }),
-  withHandlers({
-    pickImage: props => () => {
-      const { uploadImage } = props
-      ImagePicker.showImagePicker(imagePickerOptions, response => {
-        if (response.didCancel) {
-          console.log('User cancelled image picker')
-        } else if (response.error) {
-          console.log('ImagePicker Error: ', response.error)
-        } else if (response.customButton) {
-          console.log('User tapped custom button: ', response.customButton)
-        } else {
-          const path = Platform.OS === 'ios' ? 'file://' : ''
-          const source = response.uri.replace(path, '')
-          uploadImage(source)
-        }
-      })
-    },
     showTimelogs: props => () => {
       const { user: { _id } } = props
       Actions.timelogs({ owner: _id })
---
+++ jogging_time_tracker/App/Containers/UserImageHOC.js
@@ -0,0 +1,63 @@
+/* @flow*/
+import { compose, withHandlers, withState } from 'recompose'
+import { Platform, Alert } from 'react-native'
+import { RNS3 } from 'react-native-aws3'
+import ImageResizer from 'react-native-image-resizer'
+import ImagePicker from 'react-native-image-picker'
+
+const imagePickerOptions = {
+  title: 'Select Product Image',
+  storageOptions: {
+    cameraRoll: true,
+  },
+}
+
+const UserImageHOC = compose(
+  withState('uploading', 'setUploading', false),
+  withState('imageURL', 'setImageURL', ''),
+  withHandlers({
+    uploadImage: props => source => {
+      const { setImageURL, user: { _id } } = props
+      const file = {
+        uri: source,
+        name: `${_id}.png`,
+        type: 'image/png',
+      }
+
+      const options = {
+        keyPrefix: 'uploads/',
+        bucket: 'toptal-jogging-timelogger',
+        region: 'ap-southeast-2',
+        accessKey: 'AKIAIDQZRFHOIUE7SL5A',
+        secretKey: '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',
+        successActionStatus: 201,
+      }
+
+      RNS3.put(file, options).then(response => {
+        if (response.status !== 201)
+          throw new Error('Failed to upload image to S3')
+        console.log(response.body)
+      })
+    },
+  }),
+  withHandlers({
+    pickImage: props => () => {
+      const { uploadImage } = props
+      ImagePicker.showImagePicker(imagePickerOptions, response => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker')
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error)
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton)
+        } else {
+          const path = Platform.OS === 'ios' ? 'file://' : ''
+          const source = response.uri.replace(path, '')
+          uploadImage(source)
+        }
+      })
+    },
+  }),
+)
+
+export default UserImageHOC
